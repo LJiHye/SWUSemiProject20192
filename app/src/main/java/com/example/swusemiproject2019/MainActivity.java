7238e588fc8a488470b4d56daf6a288410ac0caf
이지혜 <star4266@naver.com>
2019-07-04 19:35:48 +0900

    로그인 구현

--- app/src/main/java/com/example/swusemiproject2019/MainActivity.java
+++ app/src/main/java/com/example/swusemiproject2019/MainActivity.java
@@ -2,43 +2,74 @@ package com.example.swusemiproject2019;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.swusemiproject2019.database.Database;
+import com.example.swusemiproject2019.model.MemberModel;
+
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName(); // 클래스명 획득
+    public static final int VIEW_DETAIL = 100;
+    public static final int VIEW_SAVE = 101;
+
+    Button btnLogin, btnReg;
+    EditText txtId, txtPwd;
+    Database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = Database.getInstance(getApplicationContext());
+
+        txtId = findViewById(R.id.txtId);
+        txtPwd = findViewById(R.id.txtPwd);
+
         // 로그인 버튼
-        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin = findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO
+                // ID, PW 체크
+                if(txtId != null && txtPwd != null) {
+                    boolean check = db.checkMember(txtId.getText().toString(), txtPwd.getText().toString());
+                    Log.e(TAG, "checkMember=" + check);
+
+                    if (check) {
+                        Intent intent = new Intent(getApplicationContext(), TabActivity.class);
+                        intent.putExtra("ID", txtId.getText().toString());
+                        intent.putExtra("PW", txtPwd.getText().toString());
+
+                        MemberModel member = db.getMember(txtId.getText().toString());
+                        if(member != null) {
+                            intent.putExtra("MEMBER", member);
+                        }
+                        Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                        startActivityForResult(intent, VIEW_DETAIL);
+                    }
+                }
+                if(txtId.getText().toString().isEmpty() || txtPwd.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 모두 입력하세요.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
         // 회원가입 버튼
-        Button btnReg = findViewById(R.id.btnReg);
+        btnReg = findViewById(R.id.btnReg);
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), RegActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // 탭 테스트
-        Button btnTabTest = findViewById(R.id.btnTabTest);
-        btnTabTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TabActivity.class);
                 startActivity(intent);
             }
         });
@@ -62,5 +93,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case VIEW_DETAIL:
+                break;
+            case VIEW_SAVE:
+                // TODO
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/swusemiproject2019/RegActivity.java
+++ app/src/main/java/com/example/swusemiproject2019/RegActivity.java
@@ -1,14 +1,69 @@
 package com.example.swusemiproject2019;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.swusemiproject2019.database.Database;
+import com.example.swusemiproject2019.model.MemberModel;
 
 public class RegActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName(); // 클래스명 획득
+
+    EditText editId, editName, editPwd, editCheckPwd;
+    ImageView imgReg;
+    Button btnPic, btnReg;
+
+    Database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+
+        editId = findViewById(R.id.infoId);
+        editName = findViewById(R.id.editName);
+        editPwd = findViewById(R.id.editPwd);
+        editCheckPwd = findViewById(R.id.editCheckPwd);
+
+        imgReg = findViewById(R.id.imgReg);
+
+        btnPic = findViewById(R.id.btnPic);
+        btnReg = findViewById(R.id.btnReg);
+
+        db = Database.getInstance(getApplicationContext());
+
+        // 회원가입 버튼
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MemberModel checkMember = db.getMember(editId.getText().toString());
+                if (checkMember != null) {
+                    Toast.makeText(getApplication(),"존재하는 아이디입니다.", Toast.LENGTH_LONG).show();
+                } else {
+                    if(editPwd.getText().toString().equals(editCheckPwd.getText().toString())) {
+                        MemberModel member = new MemberModel();
+                        member.setId(editId.getText().toString());
+                        member.setPwd(editPwd.getText().toString());
+                        member.setName(editName.getText().toString());
+                        member.setProfile(imgReg.getTextDirection());
+                        Database.getInstance(getApplicationContext()).setMember(member);
+
+                        db.setMember(member);
+
+                        Toast.makeText(getApplication(), "회원 가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        Toast.makeText(getApplication(), "비밀 번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
